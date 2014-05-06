d458575fc9d92f2161f5cc6395e181770ae45817
minboost <min@minboost.com>
2014-05-06 13:22:49 -0700

    Updated tests

---
+++ test4.js
@@ -0,0 +1,8 @@
+$(function(){
+	$('head').append(
+		'<meta property="og:title" content="Google aims for the mainstream with 20 new, more powerful Chromebooks">'
+		+'<meta property="og:description" content="If you haven\'t picked up a Chromebook just yet, you might want to wait a little longer. Intel has just announced plans to roll out as many as 20 new">'
+		+'<meta property="og:image" content="http://o.aolcdn.com/hss/storage/midas/99fdcfcbb0f5d908184670502de03bc9/200095467/yeahslide_0030.jpg">'
+		+'<meta property="og:type" content="article">'
+	);
+});
